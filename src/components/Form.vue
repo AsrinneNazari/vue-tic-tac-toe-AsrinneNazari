d1935f0f716be5b37fe85601e6f5bf2104138a96
Asrinne Nazari <asse_89@hotmail.com>
2024-05-20 10:28:10 +0200

    🔥 first commit

---
+++ src/components/Form.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const userInput = ref("");
+const error = ref("");
+
+const emit = defineEmits<{
+  (e: "add", text: string): void;
+}>();
+
+const handleSubmit = () => {
+    if (userInput.value !== "") {
+    emit("add", userInput.value);
+    userInput.value = "";
+    error.value = "";
+  } else {
+    error.value = "Fyll i ett namn! ";
+  }
+  emit("add", userInput.value);
+};
+</script>
+
+<template>
+  <h3>Fyll i ditt användarnamn</h3>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" v-model="userInput" />
+    <button>Lägg till</button>
+  </form>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheGame.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { Player } from "../models/Player";
+import Form from "./Form.vue";
+
+const player = ref<Player[]>([new Player("Asrinne")]);
+console.log(player + "hej");
+
+const addPlayer = (text: string) => {
+  player.value.push(new Player(text));
+};
+
+</script>
+
+<template>
+  <h2>Tik Tac Toe spelet!</h2>
+  <Form @add="addPlayer" />
+</template>
+
+<style scoped></style>
